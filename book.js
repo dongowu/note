3cab03916d3ea94242bb012d88f8c68855e79429
dongowu <jason_w96@163.com>
2025-06-26 23:48:30 +0000

    add new book

---
+++ book.js
@@ -0,0 +1,29 @@
+{
+  "title": "我的学习笔记",
+  "author": "dongowu",
+  "language": "zh-hans",
+  "plugins": [
+    "github",
+    "anchors",
+    "splitter",
+    "prism",
+    "back-to-top-button",
+    "-sharing"
+  ],
+  "pluginsConfig": {
+    "github": {
+      "url": "https://github.com/dongowu/note"
+    },
+    "prism": {
+      "lang": {
+        "go": "Go",
+        "rust": "Rust",
+        "python": "Python"
+      }
+    },
+    "back-to-top-button": {
+      "showToTop": true,
+      "showToBottom": false
+    }
+  }
+}
